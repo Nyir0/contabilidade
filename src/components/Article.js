6b2ce4acbb763f106a08186549eaeba515ca1b8a
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2024-02-02 21:23:25 -0300

    feat:secao de artigos criada

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import "../components/Article.css";
+
+export default function Article(){
+    return(
+        <>
+        <section className="mt-28">
+            <h2>Artigos</h2>
+            {/* Slides */}
+            <div>
+
+            </div>
+        </section>
+        </>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import Article from '../components/Article';
+
 export default function Home() {
     return(
         <>
@@ -6,7 +8,7 @@ export default function Home() {
                 <img className="w-[30rem] absolute max-sm:static left-12 bottom-12" src="./images/banner1.webp" alt=""/>
             </div>
             <div className="w-[40vw] max-sm:w-full pb-24 max-sm:pb-4 max-sm:flex max-sm:flex-col max-sm:items-center">
-                <h1 className="uppercase mb-4 max-sm:text-2xl">A <span className="text-[var(--yellow-base)]">Leal contabilidade</span> é</h1>
+                <h1 className="uppercase mb-4 max-sm:text-2xl">A <span className="text-[var(--yellow-gold)]">Leal contabilidade</span> é</h1>
                 <p className="text-xl max-sm:text-center">
                 o alicerce firme que constrói confiança e clareza nas finanças, garantindo uma administração sólida e confiável para o sucesso do seu negócio
                 </p>
@@ -15,6 +17,7 @@ export default function Home() {
         <div id="btn-page-down" className="bg-[var(--yellow-base)] absolute right-40 rounded-full p-3">
             <img src="./images/icons/arrow-down.svg" alt="" width={75} height={75}/>
         </div>
+        <Article />
         </>
     )
 }
