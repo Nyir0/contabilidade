a6ea49f2abf45530d969cefa498a971bed1108c8
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2024-02-11 13:25:56 -0300

    fix:corrige respons mobile contatos

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,25 +1,25 @@
 export default function Contato(){
     return(
-        <section className="flex flex-col justify-between h-[calc(100vh-200px)] max-sm:h-[calc(100vh-100px)]">
+        <section className="flex flex-col justify-between h-[calc(100vh-200px)] max-sm:h-auto">
             <div>
                 <div className="flex max-sm:flex-col items-center mb-14">
                     <img className="mr-10 max-sm:mr-0" src="../images/icons/phone.svg" alt="" width={80} height={80}/>
-                    <div>
+                    <div className="max-sm:flex max-sm:flex-col max-sm:items-center">
                         <h3>Fone fixo:</h3>
                         <strong className="text-2xl max-sm:text-base">(11) 2086-7050</strong>
                     </div>
                 </div>
                 <div className="flex max-sm:flex-col items-center mb-14">
                     <img className="mr-10 max-sm:mr-0" src="../images/icons/whatsapp.svg" alt="" width={80} height={80}/>
-                    <div>
+                    <div className="max-sm:flex max-sm:flex-col max-sm:items-center">
                         <h3>Whatsapp:</h3>
                         <strong className="text-2xl max-sm:text-base">(11) 97269-6636</strong>
                     </div>
                 </div>
                 <div className="flex max-sm:flex-col items-center mb-14">
                     <img className="mr-10 max-sm:mr-0" src="../images/icons/email.svg" alt="" width={80} height={80}/>
-                    <div>
-                        <h3 className="max-sm:text-center">E-mail:</h3>
+                    <div className="max-sm:flex max-sm:flex-col max-sm:items-center">
+                        <h3>E-mail:</h3>
                         <strong className="text-2xl max-sm:text-base">leal.contabilidade@hotmail.com</strong>
                     </div>
                 </div>
